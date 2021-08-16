983ca9902cae9b2044d8bed7825fc02d32983849
wjdghrl11 <wjdghrl26@gmail.com>
2021-08-16 23:42:41 +0900

    chapter6

---
+++ chapter6.py
@@ -0,0 +1,85 @@
+# 숫자형
+
+# 파이썬 지원 자료형
+"""
+int : 정수
+float : 실수
+complex : 복소수
+bool : 불린(참, 거짓)
+str : 문자열(시퀀스)
+list : 리스트(시퀀스)
+tuple : 튜플(시퀀스)
+set : 집합
+dict : 사전
+"""
+
+# 데이터 타입
+str1 = "python"
+bool = True and False
+str2 = 'Anaconda'
+float = 10.0 # 10 == 10.0
+int = 7
+tuple = (7, 8, 9)
+set = {7, 8, 9}
+list = [str1, str2]
+print(list)
+
+dict = {"name" : "Machine Learning", "version" : 2.0}
+print(type(str1))
+print(type(bool))
+print(type(str2))
+print(type(float))
+print(type(int))
+print(type(dict))
+print(type(tuple))
+print(type(set))
+
+tuple = (7, 8, 9)
+set = {7, 8, 9}
+
+# 숫자형 연산자 
+"""
++
+-
+*
+/
+// : 몫
+% : 나머지만
+abs(x) : 절대값
+power(x, y) : x의 y의제곱 = x ** y
+"""
+
+i = 77
+i2 = -14
+big_int = 777777777777777777777777777777
+
+# 정수 출력
+print(i)
+print(i2)
+print(big_int)
+
+# 실수 출력
+f = 0.9999
+f2 = 3.141592
+f3 = -3.9
+f4 = 3 / 9
+print(f)
+print(f2)
+print(f3)
+print(f4)
+
+# 연산 실습
+i1 = 39
+i2 = 939
+big_int1 = 77777777777799999956858
+big_int2 = 195185969346996
+f1 = 1.234
+f2 - 3.9393
+
+print(i1 + i2)
+print(f1 + f2)
+print(big_int1 + big_int2)
+print(i1 * i2)
+print(f1 * f2)
+print(big_int1 * big_int2)
+
