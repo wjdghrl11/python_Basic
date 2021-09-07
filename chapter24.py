bbb08185ad64f8dcd89f47df6f70a6ad2523db27
wjdghrl11 <wjdghrl26@gmail.com>
2021-09-07 20:02:53 +0900

    chapter24

---
+++ chapter24.py
@@ -0,0 +1,25 @@
+# 예제4
+class Dog: # object 상속
+    # 클래스 속성
+    species = 'firstdog'
+    
+    # 초기화/인스턴스 속성
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        return '{} is {} years old'.format(self.name, self.age)
+
+    def speak(self, sound):
+        return "{} says {}!".format(self.name, sound)
+
+# 인스턴스 생성
+c = Dog('july', 4)
+d = Dog('Marry', 10)
+
+# 메소드 호출
+print(c.info())
+
+print(c.speak('wal wal'))
+print(d.speak('Mung Mung'))
